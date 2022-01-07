de7619aab34279975476663e77e0abf037d45cbe
Rishi16011601 <asijarishiwar@gmail.com>
2022-01-07 20:57:57 +0530

    b2 commit 1

--- Assets/Scripts/BlockNew.cs
+++ Assets/Scripts/BlockNew.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockNew : Block //INHERITENCE
+{
+    public override void TriggerSparklesVFX()  //POLYMORPHISM 
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
+    }
+}
